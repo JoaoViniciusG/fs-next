04fba1036d02fa0d06f92ea13b2538b6d5dec798
Beatriztesser <beatrizeduardabt@gmail>
2025-02-11 10:05:18 -0400

    aula fs 11/02

--- src/app/interno/pedidos/criar_pedidos/page.jsx
+++ src/app/interno/pedidos/criar_pedidos/page.jsx
@@ -2,15 +2,28 @@
 
 import StandardButton from "@/components/buttons/stardardButton/standardButton";
 import BasicScreen from "@/components/screens/basicScreen/basicScreen";
+import BorderContainer from "@/components/containers/borderContainer/page";
+import InputLabel from "@/components/inputs/inputLabel/inputLabel";
+import styles from "./page.module.css"
 
 
 export default function PageCriarPedidos() {
   return (
     <BasicScreen pageTitle="Criar pedido">
-      <div style={{height: "70vh", width: "100%", flex: 1, backgroundColor: "blue"}}>
-      </div>
-
-      <StandardButton text="ALTERAR" hoverColor="var(--orange)" callback={() => {}}/>
+      <BorderContainer title= "Dados do cliente: " padding-top= "30px">
+      <div class="campo-busca">
+                        <label>
+                            <h3 class="title-input">Buscar o cliente:</h3>
+                            <div class="input-wrapper">
+                                <input type="text" class="input buscar-cliente"/>
+                                <button type="button" class="botao-lupa">
+                                    <img src="../img/Lupa.png" alt="Lupa" class="icon-lupa">
+                                </button>
+                            </div>
+                        </label>
+                    </div>
+      </BorderContainer>
+  
     </BasicScreen>
   );
 }
@@ -18,142 +31,3 @@ export default function PageCriarPedidos() {
 
 
 
-{/* <div class="div-content-cliente">
-                    <div class="div-title-border">
-                        <h2>Dados do cliente:</h2>
-                    </div>
-                    
-                    <div class="campo-busca">
-                        <label>
-                            <h3 class="title-input">Buscar o cliente:</h3>
-                            <div class="input-wrapper">
-                                <input type="text" class="input buscar-cliente">
-                                <button type="button" class="botao-lupa">
-                                    <img src="../img/Lupa.png" alt="Lupa" class="icon-lupa">
-                                </button>
-                            </div>
-                        </label>
-                    </div>
-                    <div class="linha">
-                        <label>
-                            <h3 class="title-input">Nome:</h3>
-                            <input type="text" class="campo-nome input">
-                        </label>
-
-                        <label>
-                            <h3 class="title-input">CPF/CNPJ:</h3>
-                            <input type="text" class="campo-cpf input">
-                        </label>
-                    </div>
-
-                    <div class="linha">
-                        <label>
-                            <h3 class="title-input">Endereço:</h3>
-                            <input type="text" class="campo-endereco input">
-                        </label>
-
-                        <label >
-                            <h3 class="title-input">Bairro:</h3>
-                            <input type="text" class="campo-bairro input">
-                        </label>
-
-                        <label>
-                            <h3 class="title-input">CEP:</h3>
-                            <input type="text" class="campo-cep input">
-                        </label>
-                    </div>
-
-  
-                    <div class="linha">
-                        <label>
-                            <h3 class="title-input">Cidade:</h3>
-                            <input type="text" class="campo-cidade input">
-                        </label>
-
-                        <label>
-                            <h3 class="title-input">Estado:</h3>
-                            <input type="text" class="campo-estado input">
-                        </label>
-
-                        <label>
-                            <h3 class="title-input">Telefone:</h3>
-                            <input type="text" class="campo-telefone input">
-                        </label>
-                    </div>       
-                </div>
-
-                <fieldset class="field-master-component">
-                    <legend>Dados do pedido:</legend>
-
-                    <div class="container-header-list-options">
-                        <button class="button-header-options adicionar" >ADICIONAR PRODUTO</button>
-                        <button class="button-header-options alterar " >ALTERAR PRODUTO</button>
-                        <button class="button-header-options excluir" >EXCLUIR PRODUTO</button>
-                    </div>
-                    <div class="table-products">
-                        <div class="header-list-products">
-                            <p class="list-header-title">Cód. do Produto</p>
-                            <p class="list-header-title">Nome do Produto / Modelo</p>
-                            <p class="list-header-title">Marca</p>
-                            <p class="list-header-title">Quantidade</p>
-                            <p class="list-header-title">Valor Unit.</p>
-                            <p class="list-header-title">Subtotal</p>
-                        </div>
-
-                        <div class="div-table-container-content">
-                            <div>
-                              <hr class="hr-border" style="left: 15%;">  
-                              <hr class="hr-border" style="left: 45%;">  
-                              <hr class="hr-border" style="left: 55%;">  
-                              <hr class="hr-border" style="left: 75%;"> 
-                              <hr class="hr-border" style="left: 87%;">  
-                            </div>
-                            <table class="table-container-content">
-                                <tr>
-                                    <th style="width: 15%; background-color: red;"></th>
-                                    <th style="width: 30%; background-color: green;"></th>
-                                    <th style="width: 10%; background-color: red;"></th>
-                                    <th style="width: 20%; background-color: green;"></th>
-                                    <th style="width: 12%; background-color: red;"></th>
-                                    <th style="width: 13%; background-color: green;"></th>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </fieldset>
-
-                <fieldset class="field-master-component field-total">
-                    <div class="div-content-input-total">
-                        <label class="label-input-login label-medium">
-                            <span>Subtotal:</span>
-                            <input id="input-subtotal" type="text" readonly/>
-                        </label>
-                        <label class="label-input-login label-medium">
-                            <span>Desconto:</span>
-                            <input id="input-desconto" type="text" readonly/>
-                        </label>
-                        <label class="label-input-login label-medium">
-                            <span>Total:</span>
-                            <input id="input-total" type="text" readonly/>
-                        </label>
-                    </div>
-
-                    <label class="label-input-login label-medium">
-                        <span>Observação:</span>
-                        <textarea id="input-total" type="text" placeholder="Escreva sua descrição..."></textarea>
-                    </label>
-
-                </fieldset>
-
-                <div class="container-infos-bottom">
-                    <div class="div-tag-bottom">
-                        <p>Código do pedido:</p>
-
-                        <span>N° 000</span>
-                    </div>
-
-                    <button class="button-header-options criar_pedido" >CRIAR PEDIDO</button>
-                </div>
-            </div> 
-        </div>
-    </div> */}
