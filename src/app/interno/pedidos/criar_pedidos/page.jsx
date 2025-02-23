45fe60581b921ac19f1f67093eec5042b59746da
JoaoViniciusG <joaovigu07@gmail.com>
2025-02-22 21:48:09 -0400

    Resolvendo problemas .1

--- src/app/interno/pedidos/criar_pedidos/page.jsx
+++ src/app/interno/pedidos/criar_pedidos/page.jsx
@@ -10,51 +10,52 @@ import { useState } from "react";
 export default function PageCriarPedidos() {
 
   const [nome, setNome] = useState("");
-    const [cpfCnpj, setCpfCnpj] = useState("");
-    const [endereco, setEndereco] = useState("");
-    const [bairro, setBairro] = useState("");
-    const [cep, setCep] = useState("");
-    const [cidade, setCidade] = useState("");
-    const [estado, setEstado] = useState("");
-    const [telefone, setTelefone] = useState("");
+  const [cpfCnpj, setCpfCnpj] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cep, setCep] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [estado, setEstado] = useState("");
+  const [telefone, setTelefone] = useState("");
 
-    const [subtotal, setSubtotal] = useState("");
-    const [desconto, setDesconto] = useState("");
-    const [total, setTotal] = useState("");
-    const [observacao, setObservacao] = useState("");
-    
-    return (
-      <BasicScreen pageTitle="Criar pedido">
-        <BorderContainer title="Dados do cliente">
-          <div className={styles.dvInputs}>
-            <div className={styles.linha}>
-                <InputLabel className={styles.label} label="Nome:" value={nome} setValue={setNome}  width= "80%" style={{flex:1}}/>
-                <InputLabel className={styles.label} label="CPF/CNPJ:" value={cpfCnpj} setValue={setCpfCnpj}  width= "80%" style={{flex:1}} />
-            </div>
+  const [subtotal, setSubtotal] = useState("");
+  const [desconto, setDesconto] = useState("");
+  const [total, setTotal] = useState("");
+  const [observacao, setObservacao] = useState("");
 
-            <div className={styles.linha}>
-                <InputLabel className={styles.label} label="Endereço:" value={endereco} setValue={setEndereco}  width="90%" style={{flex:1}} />
-                <InputLabel className={styles.label} label="Bairro:" value={bairro} setValue={setBairro}  width="80%" style={{flex:1}} />
-                <InputLabel className={styles.label} label="CEP:" value={cep} setValue={setCep}  width="70%" style={{flex:1}} />
-            </div>
+  return (
+    <BasicScreen pageTitle="Criar pedido">
+      <BorderContainer title="Dados do cliente">
+        <div className={styles.dvInputs}>
+          <div className={styles.linha}>
+            <InputLabel className={styles.label} label="Nome:" value={nome} setValue={setNome} width="80%" style={{ flex: 1 }} />
+            <InputLabel className={styles.label} label="CPF/CNPJ:" value={cpfCnpj} setValue={setCpfCnpj} width="80%" style={{ flex: 1 }} />
+          </div>
 
-            <div className={styles.linha}>
-                <InputLabel  className={styles.label} label="Cidade:" value={cidade} setValue={setCidade}  width="90%" style={{flex:1}} />
-                <InputLabel className={styles.label} label="Estado:" value={estado} setValue={setEstado} width="90%" style={{flex:1}}/>
-                <InputLabel className={styles.label} label="Telefone:" value={telefone} setValue={setTelefone}  width="90%" style={{flex:1}} />
-            </div>
+          <div className={styles.linha}>
+            <InputLabel className={styles.label} label="Endereço:" value={endereco} setValue={setEndereco} width="90%" style={{ flex: 1 }} />
+            <InputLabel className={styles.label} label="Bairro:" value={bairro} setValue={setBairro} width="80%" style={{ flex: 1 }} />
+            <InputLabel className={styles.label} label="CEP:" value={cep} setValue={setCep} width="70%" style={{ flex: 1 }} />
+          </div>
+
+          <div className={styles.linha}>
+            <InputLabel className={styles.label} label="Cidade:" value={cidade} setValue={setCidade} width="90%" style={{ flex: 1 }} />
+            <InputLabel className={styles.label} label="Estado:" value={estado} setValue={setEstado} width="90%" style={{ flex: 1 }} />
+            <InputLabel className={styles.label} label="Telefone:" value={telefone} setValue={setTelefone} width="90%" style={{ flex: 1 }} />
+          </div>
         </div>
-        </BorderContainer>
+      </BorderContainer>
 
 
-        <BorderContainer title={"Dados do pedido:"}>
-            <div className={styles.containerHeaderListOptions}>
-              <StandardButton className={styles.buttonHeaderOptions} text="ADICIONAR PRODUTO" hoverColor="var(--cyan)"></StandardButton>
-              <StandardButton className={styles.buttonHeaderOptions} text="ALTERAR PRODUTO" hoverColor="var(--cadetblue-ligtht)"></StandardButton>
-              <StandardButton className={styles.buttonHeaderOptions} text= "EXCLUIR PRODUTO" hoverColor="var(--darkred)"> </StandardButton>
-            </div>
+      <BorderContainer title={"Dados do pedido:"} className={styles.borderContainer}>
+        <div className={styles.containerDataMaster}>
+          <div className={styles.containerHeaderListOptions}>
+            <StandardButton className={styles.buttonHeaderOptions} text="ADICIONAR PRODUTO" hoverColor="var(--cyan)"></StandardButton>
+            <StandardButton className={styles.buttonHeaderOptions} text="ALTERAR PRODUTO" hoverColor="var(--cadetblue-ligtht)"></StandardButton>
+            <StandardButton className={styles.buttonHeaderOptions} text="EXCLUIR PRODUTO" hoverColor="var(--darkred)"> </StandardButton>
+          </div>
 
-            <div className={styles.tableProducts}>
+          <div className={styles.tableProducts}>
             <div className={styles.headerListProducts}>
               <p className={styles.listHeaderTitle}>Cód. do Produto</p>
               <p className={styles.listHeaderTitle}>Nome do Produto / Modelo</p>
@@ -74,60 +75,61 @@ export default function PageCriarPedidos() {
               </div>
               <table className={styles.tableContainerContent}>
                 <thead>
-                <tr>
-                  <th style={{ width: '15%' }}></th>
-                  <th style={{ width: '30%' }}></th>
-                  <th style={{ width: '10%' }}></th>
-                  <th style={{ width: '20%' }}></th>
-                  <th style={{ width: '12%' }}></th>
-                  <th style={{ width: '13%' }}></th>
-                </tr>
+                  <tr>
+                    <th style={{ width: '15%' }}></th>
+                    <th style={{ width: '30%' }}></th>
+                    <th style={{ width: '10%' }}></th>
+                    <th style={{ width: '20%' }}></th>
+                    <th style={{ width: '12%' }}></th>
+                    <th style={{ width: '13%' }}></th>
+                  </tr>
                 </thead>
               </table>
             </div>
-          </div> 
-        </BorderContainer>
+          </div>
+        </div>
+      </BorderContainer>
 
-        <BorderContainer> 
-          <div className={styles.divContentInputTotal}>
-            <div className={styles.linha}>
-            <InputLabel 
-            label="Subtotal" 
-            value={subtotal} 
-            setValue={setSubtotal} 
-            className={styles.inputDadosPessoais} 
-            readonly={true} 
-            width= "80%"
-          />
-          <InputLabel 
-            label="Desconto" 
-            value={desconto} 
-            setValue={setDesconto} 
-            className={styles.inputDadosPessoais} 
-            readonly={true} 
-            width= "80%"
-          />
-          <InputLabel 
-            label="Total" 
-            value={total} 
-            setValue={setTotal} 
-            className={styles.inputDadosPessoais} 
-            readonly={true} 
-            width= "80%"
-          />
-            </div>
-          
-            <label className={styles.labelInputLogin }>
-                  <span>Observação:</span>
-                  <textarea className={styles.textarea} type="text" ></textarea>
-            </label>
-              </div>
-        </BorderContainer>
+      <BorderContainer>
+        <div className={styles.divContentInputTotal}>
+          <div className={styles.linha}>
+            <InputLabel
+              label="Subtotal"
+              value={subtotal}
+              setValue={setSubtotal}
+              className={styles.inputDadosPessoais}
+              readonly={true}
+              width="80%"
+            />
+            <InputLabel
+              label="Desconto"
+              value={desconto}
+              setValue={setDesconto}
+              className={styles.inputDadosPessoais}
+              readonly={true}
+              width="80%"
+            />
+            <InputLabel
+              label="Total"
+              value={total}
+              setValue={setTotal}
+              className={styles.inputDadosPessoais}
+              readonly={true}
+              width="80%"
+            />
+          </div>
 
-      </BasicScreen>
-    );
-  }
-  
+          <label className={styles.labelInputLogin}>
+            <span>Observação:</span>
+            <textarea className={styles.textarea} type="text" ></textarea>
+          </label>
+        </div>
+      </BorderContainer>
+
+    </BasicScreen>
+  );
+}
+
 
 
 
--- src/components/containers/borderContainer/page.jsx
+++ src/components/containers/borderContainer/page.jsx
@@ -1,8 +1,8 @@
 import styles from './page.module.css';
 
-export default function BorderContainer({ title, style, children}) {
+export default function BorderContainer({ title, style, children, className}) {
     return (
-        <div className={styles.containerMaster} style={style}>
+        <div className={`${styles.containerMaster} ${className}`} style={style}>
             <h1 style={{display: (title == null) ? "none" : "block" }} className={styles.titleContainer}><span>{title}</span></h1>
             
             <div className={styles.contentContainer}>{ children }</div>
