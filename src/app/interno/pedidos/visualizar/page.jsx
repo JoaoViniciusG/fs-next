6d5d59b9c15802b92dc03250e277a36ba0b257e5
Beatriztesser <beatrizeduardabt@gmail.com>
2025-05-11 16:28:36 -0400

    resolvido

--- src/app/interno/pedidos/visualizar/page.jsx
+++ src/app/interno/pedidos/visualizar/page.jsx
@@ -134,7 +134,7 @@ export default function PageAlterarPedidos() {
                           <span>N° 000</span>
                       </div>
 
-                  <Link href='/interno/pedidos/alterar_pedido'>
+                  <Link href='/interno/pedidos/alterar'>
                         <StandardButton  text="ALTERAR PEDIDO" hoverColor="var(--cadetblue-ligtht)"></StandardButton>    
                   </Link>
               </div>
